9b101e8364ac3a4d6129d3b847d6f4dbda1942d7
Dimitar <dimitar.toshev@mail.utoronto.ca>
2024-11-24 20:20:09 -0500

    Added SaveArticle use case CA files

---
+++ src/main/java/interface_adapter/logged_in/SaveArticleController.java
@@ -0,0 +1,25 @@
+package interface_adapter.logged_in;
+
+import entity.Article;
+import use_case.save_article.SaveArticleInputBoundary;
+import use_case.save_article.SaveArticleInputData;
+
+/**
+ * The controller for the SaveArticle Use Case.
+ */
+public class SaveArticleController {
+    private final SaveArticleInputBoundary saveArticleUseCaseInteractor;
+
+    public SaveArticleController(SaveArticleInputBoundary saveArticleUseCaseInteractor) {
+        this.saveArticleUseCaseInteractor = saveArticleUseCaseInteractor;
+    }
+
+    /**
+     * Executes the SaveArticle Use Case.
+     * @param article the category to add
+     */
+    public void execute(Article article) {
+        final SaveArticleInputData saveArticleInputData = new SaveArticleInputData(article);
+        saveArticleUseCaseInteractor.execute(saveArticleInputData);
+    }
+}
